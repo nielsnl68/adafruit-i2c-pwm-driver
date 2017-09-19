1eec1acbcc5e89a487f771916a65227682b2855d
nielsnl68 <nielsnl68@gmail.com>
2017-09-19 18:07:09 +0200

    fixed typos

--- src/i2cWrapper.js
+++ src/i2cWrapper.js
@@ -1,51 +1,52 @@
-const I2C = require('i2c-bus')
+'use strict';
+
+var I2C = require('i2c-bus');
 
 /*
   this wrappers wraps the i2c readBytes, writeBytes functions and returns promises
 */
-function I2CWrapper (port, address, debug) {
-  const i2c = I2C.openSync(port);
-  const address = address;
-  const debug = debug;
-    
-  const readBytes = (cmd, length) => {
-	let buf = Buffer.alloc(length)
-    return new Promise(
-      function (resolve, reject) {
-        i2c.i2cRead(this.address, cmd, length, buf, function (error, data) {
-          if (error) {
-            return reject(error)
-          }
-          resolve(data)
-        })
-      }
-    )
-  }
+function makeI2CWrapper(address, _ref) {
+  var device = _ref.device,
+      debug = _ref.debug;
+      
 
-  const writeBytes = (cmd, buf) => {
+  var i2c = new I2C.openSync(1);
+
+  var readBytes = function readBytes(cmd, length) {
+    return new Promise(function (resolve, reject) {
+      let buf = Buffer.alloc(length);
+      i2c.readBytes(address, cmd, length, buf, function (error, data) {
+        if (error) {
+          return reject(error);
+        }
+        resolve(data);
+      });
+    });
+  };
+
+  var writeBytes = function writeBytes(cmd, buf) {
     if (!(buf instanceof Array)) {
-      buf = [buf]
+      buf = [buf];
     }
-	buf = new Buffer(buf);
-    if(debug){
-      console.log(`cmd ${cmd.toString(16)} values ${buf}`)
+    buf = Buffer(buf);
+    if (debug) {
+      console.log('cmd ' + cmd.toString(16) + ' values ' + buf);
     }
-    return new Promise(
-      function (resolve, reject) {
-        i2c.i2cWrite(this.address, cmd, buf.length, buf, function (error, data) {
-          if (error) {
-            return reject(error)
-          }
-          resolve(data)
-        })
-      }
-    )
-  }
+    return new Promise(function (resolve, reject) {
+      i2c.writeBytes(cmd, buf.length, buf, function (error, data) {
+        if (error) {
+          return reject(error);
+        }
+
+        resolve(data);
+      });
+    });
+  };
 
   return {
-    readBytes,
-    writeBytes
-  }
+    readBytes: readBytes,
+    writeBytes: writeBytes
+  };
 }
 
-module.exports = I2CWrapper
+module.exports = makeI2CWrapper;
